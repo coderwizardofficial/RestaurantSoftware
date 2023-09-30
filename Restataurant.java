5e1431a744cea3c6626afe430f5a518888e43467
Coderwizard Inc <104607577+coderwizardofficial@users.noreply.github.com>
2023-09-30 13:12:44 -0600

    Add files via upload
    Create Restaurant Software Code

---
+++ Restataurant.java
@@ -0,0 +1,12 @@
+public class Restataurant {
+    public static void main(String[] args) {
+
+        String Menu;
+        String Name;
+        Addr Address;
+        Empl Emplyoer;
+        Cust Customer;
+
+        System.out.println();
+    }
+}
